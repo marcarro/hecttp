051139d0eb8b771b3dd094774db19f2b60d7ad4d
Hector Arroyo <604121a1-71b4-441f-9460-49b3e366ecc5@anonaddy.me>
2022-07-28 18:52:48 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
